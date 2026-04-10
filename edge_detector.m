cf8d74c40aba7ee78517e75e402bc142e4054270
Mohammad Sadeghi <mohammad2184@gmail.com>
2026-04-09 20:28:48 -0700

    modified low and hight of edge function.

--- edge_detector.m
+++ edge_detector.m
@@ -6,5 +6,5 @@ BW_canny_2 = edge(I, 'canny', [0.02 0.2], sqrt(2));
 
 figure;
 subplot(1, 3, 1); imshow(I); title('Original');
-subplot(1, 3, 2); imshow(BW_canny_1); title('Default');
-subplot(1, 3, 3); imshow(BW_canny_2); title('[0.5 1.0]');
+subplot(1, 3, 2); imshow(BW_canny_1); title('Canny (Default)');
+subplot(1, 3, 3); imshow(BW_canny_2); title('Canny (Optimized (0.02 0.2))');
